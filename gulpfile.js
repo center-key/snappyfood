e1916221b91fdb92e6dfa7ccafe328e4a2689f3f
Dem Pilafian <dem@centerkey.com>
2017-09-06 00:10:21 -0700

    Consistent variable names

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@
 const gulp =     require('gulp');
 const htmlHint = require('gulp-htmlhint');
 const size =     require('gulp-size');
-const w3cjs =    require('gulp-w3cjs');
+const w3cJs =    require('gulp-w3cjs');
 
 const htmlHintConfig = { 'attr-value-double-quotes': false };
 
 function validateHtml() {
    return gulp.src('websites/**/*.html')
       .pipe(size({ showFiles: true }))
-      .pipe(w3cjs())
-      .pipe(w3cjs.reporter())
+      .pipe(w3cJs())
+      .pipe(w3cJs.reporter())
       .pipe(htmlHint(htmlHintConfig))
       .pipe(htmlHint.reporter());
    }
